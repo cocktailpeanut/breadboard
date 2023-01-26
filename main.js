a952e1ea0c57a0d2c97e79d959fbfbb306695191
cocktailpeanut <cocktailpeanuts@proton.me>
2023-01-26 08:12:33 -0500

    Built with Breadmachine
    Use breadmachine as backend. Extracted out 99% of the original logic out to breadmachine, and now simply wrapping the breadmachine with an electron wrapper.

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
 const {app, shell, BrowserWindow, ipcMain, dialog, session, clipboard } = require('electron')
-const { fdir } = require("fdir");
 const contextMenu = require('electron-context-menu');
-const path = require('path')
-const xmlFormatter = require('xml-formatter');
-const fs = require('fs')
-const os = require('os')
-const express = require('express')
-const fastq = require('fastq')
-const getport = require('getport')
-const Diffusionbee = require('./crawler/diffusionbee')
-const Standard = require('./crawler/standard')
-const Updater = require('./updater/index')
-const GM = require('./crawler/gm')
-const gm = new GM()
+const path = require("path")
+const Breadmachine = require('breadmachine')
 const is_mac = process.platform.startsWith("darwin")
 contextMenu({ showSaveImageAs: true });
 var mainWindow;
 var theme = "default";
-const updater = new Updater()
 const titleBarOverlay = (theme) => {
   if (is_mac) {
     return false
@@ -47,7 +35,6 @@ function createWindow (port) {
       preload: path.join(__dirname, 'preload.js')
     },
   })
-//  mainWindow.webContents.openDevTools()
   mainWindow.loadURL(`http://localhost:${port}`)
   mainWindow.maximize();
 
@@ -59,272 +46,50 @@ function createWindow (port) {
     return { action: 'deny' };
   });
 }
-var need_update = null
-var default_sync_mode = "default"    // only synchronize once at launch
-var current_sorter_code = 0
-
-
-const VERSION = app.getVersion()
-console.log("VERSION", VERSION)
-
-const releaseFeed = "https://github.com/cocktailpeanut/breadboard/releases.atom"
-const releaseURL = "https://github.com/cocktailpeanut/breadboard/releases"
-const updateCheck = async () => {
-  let res = await updater.check(releaseFeed)
-  console.log("Feed", res)
-  if (res.feed && res.feed.entry) {
-
-    let latest = (Array.isArray(res.feed.entry) ? res.feed.entry[0] : res.feed.entry)
-    if (latest.title === VERSION) {
-      console.log("UP TO DATE", latest.title, VERSION)
-    } else {
-      console.log("Need to update to", latest)
-      need_update = {
-        $url: releaseURL,
-        latest
-      }
-    }
-  }
-}
+const breadmachine = new Breadmachine();
 app.whenReady().then(async () => {
-  await updateCheck().catch((e) => {
-    console.log("update check error", e)
-  })
-
-//  session.defaultSession.clearStorageData()   // for testing freshly every time
-  const port = await new Promise((resolve, reject) => {
-    getport(function (e, p) {
-      if (e) throw e
-      resolve(p)
-    })
-  })
-
-  const server = express()
-  server.use(express.static(path.resolve(__dirname, 'public')))
-  server.use("/docs", express.static(path.resolve(__dirname, 'docs')))
-  server.set('view engine', 'ejs');
-  server.set('views', path.resolve(__dirname, "views"))
-  server.get("/", async (req, res) => {
-    let sync_mode = (req.query.synchronize ? req.query.synchronize : default_sync_mode)
-    let sync_folder = (req.query.sync_folder ? req.query.sync_folder : "")
-    if (req.query && req.query.sorter_code) {
-      current_sorter_code = req.query.sorter_code
-    }
-    res.render("index", {
-      platform: process.platform,
-      query: req.query,
-      version: VERSION,
-      sync_mode,
-      sync_folder,
-      need_update,
-      current_sorter_code,
-      theme
-    })
-    if (default_sync_mode) default_sync_mode = false   // disable sync after the first time at launch
-  })
-  server.get("/settings", (req, res) => {
-    res.render("settings", {
-      platform: process.platform,
-      version: VERSION,
-      query: req.query,
-      theme
-    })
-  })
-  server.get("/favorites", (req, res) => {
-    res.render("favorites", {
-      platform: process.platform,
-      version: VERSION,
-      theme
-    })
-  })
-  server.get("/help", (req, res) => {
-    let items = [{
-      name: "discord",
-      description: "ask questions and share feedback",
-      icon: "fa-brands fa-discord",
-      href: "https://discord.gg/6MJ6MQScnX"
-    }, {
-      name: "twitter",
-      description: "stay updated on Twitter",
-      icon: "fa-brands fa-twitter",
-      href: "https://twitter.com/cocktailpeanut"
-    }, {
-      name: "github",
-      description: "feature requests and bug report",
-      icon: "fa-brands fa-github",
-      href: "https://github.com/cocktailpeanut/breadboard/issues"
-    }]
-    res.render("help", {
-      items,
-      platform: process.platform,
-      version: VERSION,
-      theme
-    })
-  })
-  server.get('/file', (req, res) => {
-    res.sendFile(req.query.file)
-  })
-  server.listen(port, () => {
-    console.log(`Breadboard listening on port ${port}`)
-  })
-  ipcMain.handle('sync', async (event, rpc) => {
-    console.log("## sync from rpc", rpc)
-    let filter
-    const queue = fastq.promise(async (msg) => {
-      mainWindow.webContents.send('msg', msg)
-    }, 1)
-    if (rpc.paths) {
-      let diffusionbee;
-      let standard;
-      for(let i=0; i<rpc.paths.length; i++) {
-        let { file_path, root_path } = rpc.paths[i]
-        let res;
-        try {
-          if (/diffusionbee/g.test(root_path)) {
-            if (!diffusionbee) {
-              diffusionbee = new Diffusionbee(root_path)
-              await diffusionbee.init()
-            }
-            res = await diffusionbee.sync(file_path, rpc.force)
-          } else {
-            if (!standard) {
-              standard = new Standard(root_path)
-              await standard.init()
-            }
-            res = await standard.sync(file_path, rpc.force)
-          }
-        } catch (e) {
-          console.log("E", e)
-        }
-        if (res) {
-          await queue.push({
-            app: root_path,
-            total: rpc.paths.length,
-            progress: i,
-            meta: res
-          })
-        } else {
-          await queue.push({
-            app: root_path,
-            total: rpc.paths.length,
-            progress: i,
-          })
-        }
-      }
-    } else if (rpc.root_path) {
-      let filenames = await new fdir()
-        .glob("**/*.png")
-        .withBasePath()
-        .crawl(rpc.root_path)
-        .withPromise()
-
-      if (filenames.length > 0) {
-        let crawler;
-        if (/diffusionbee/g.test(rpc.root_path)) {
-          crawler = new Diffusionbee(rpc.root_path)
-        } else {
-          crawler = new Standard(rpc.root_path)
-        }
-        await crawler.init()
-        for(let i=0; i<filenames.length; i++) {
-          let filename = filenames[i]
-          let stat = await fs.promises.stat(filename)
-          let btime = new Date(stat.birthtime).getTime()
-          if (!rpc.checkpoint || btime > rpc.checkpoint) {
-            //console.log("above checkpoint", btime, rpc.checkpoint, filename)
-            let res = await crawler.sync(filename, rpc.force)
-            if (res) {
-              if (!res.btime) res.btime = res.mtime
-              await queue.push({
-                app: rpc.root_path,
-                total: filenames.length,
-                progress: i,
-                meta: res
-              })
-              continue;
-            }
-          }
-          await queue.push({
-            app: rpc.root_path,
-            total: filenames.length,
-            progress: i,
-          })
-        }
-      } else {
-        await queue.push({
-          app: rpc.root_path,
-          total: 1,
-          progress: 1,
-        })
-      }
+  await breadmachine.init({
+    theme,
+    config: path.resolve(__dirname, "breadboard.yaml"),
+    releases: {
+      feed: "https://github.com/cocktailpeanut/breadboard/releases.atom",
+      url: "https://github.com/cocktailpeanut/breadboard/releases"
     }
   })
-  ipcMain.handle("theme", (event, _theme) => {
-    theme = _theme
+
+  // Request handlers for api.js
+  breadmachine.ipc.handle("theme", (event, _theme) => {
+    breadmachine.ipc.theme = _theme
     if (mainWindow.setTitleBarOverlay) {
-      mainWindow.setTitleBarOverlay(titleBarOverlay(theme))
+      mainWindow.setTitleBarOverlay(titleBarOverlay(breadmachine.ipc.theme))
     }
   })
-  ipcMain.handle('del', async (event, filenames) => {
-    for(filename of filenames) {
-      console.log("deleting", filename)
-      await fs.promises.rm(filename).catch((e) => {
-        console.log("error", e)
-      })
-    }
+  breadmachine.ipc.handle('debug', (event) => {
+    mainWindow.webContents.openDevTools()
   })
-  ipcMain.on('ondragstart', (event, filePaths) => {
-    event.sender.startDrag({
-      files: filePaths,
-      icon: filePaths[0],
-    })
-  })
-  ipcMain.handle('select', async (event) => {
+  breadmachine.ipc.handle('select', async (event) => {
     let res = await dialog.showOpenDialog({ properties: ['openDirectory', 'showHiddenFiles'] })
     if (!res.canceled && res.filePaths && res.filePaths.length > 0) {
       return res.filePaths
     }
   })
-  ipcMain.handle('defaults', async (event) => {
-    let home = os.homedir()
-    return [
-      path.resolve(home, "invokeai", "outputs"),
-      path.resolve(home, ".diffusionbee", "images"),
-    ]
-  })
-  ipcMain.handle('copy', (event, text) => {
-    clipboard.writeText(text)
-  })
-  ipcMain.handle('gm', async (event, rpc) => {
-    if (rpc.cmd === "set" || rpc.cmd === "rm") {
-      let res = await gm[rpc.cmd](...rpc.args)
-      return res
-    } 
-  })
-  ipcMain.handle('open', async (event, file_path) => {
+  breadmachine.ipc.handle('open', async (event, file_path) => {
     await shell.showItemInFolder(file_path)
   })
-  ipcMain.handle('xmp', async (event, file_path) => {
-    let res = await gm.get(file_path)
-    return xmlFormatter(res.chunk.data.replace("XML:com.adobe.xmp\x00\x00\x00\x00\x00", ""), {
-      indentation: "  "
-    })
-  })
-  ipcMain.handle('docs', async (event, file_path) => {
-    let modal = new BrowserWindow({
-      parent: mainWindow,
-//      modal: true
-    })
-    modal.loadURL(`http://localhost:${port}/docs/doc.html`)
-  })
-  ipcMain.handle('debug', (event) => {
-    mainWindow.webContents.openDevTools()
+
+  // Request handlers for preload.js
+  ipcMain.handle("ondragstart", (event, filePaths) => {
+    if (event && event.sender) {
+      event.sender.startDrag({
+        files: filePaths,
+        icon: filePaths[0]
+      })
+    }
   })
 
-  createWindow(port)
-//  synchronize()
+  createWindow(breadmachine.port)
   app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow(port)
+    if (BrowserWindow.getAllWindows().length === 0) createWindow(breadmachine.port)
   })
 
 })
--- preload.js
+++ preload.js
@@ -1,54 +1,6 @@
 const { contextBridge, webFrame, ipcRenderer } = require('electron')
-contextBridge.exposeInMainWorld('electronAPI',{
-  sync: (rpc) => {
-    return ipcRenderer.invoke('sync', rpc)
-  },
-  del: (filenames) => {
-    if (Array.isArray(filenames)) {
-      return ipcRenderer.invoke("del", filenames)
-    } else {
-      return ipcRenderer.invoke("del", [filenames])
-    }
-  },
+contextBridge.exposeInMainWorld('electronAPI', {
   startDrag: (fileNames) => {
-    ipcRenderer.send('ondragstart', fileNames)
+    ipcRenderer.invoke('ondragstart', fileNames)
   },
-  onMsg: (callback) => ipcRenderer.on('msg', callback),
-  select: () => {
-    return ipcRenderer.invoke("select")
-  },
-  copy: (text) => {
-    return ipcRenderer.invoke("copy", text)
-  },
-  defaults: () => {
-    return ipcRenderer.invoke("defaults")
-  },
-  gm: (rpc) => {
-    return ipcRenderer.invoke("gm", rpc)
-  },
-  open: (file_path) => {
-    return ipcRenderer.invoke("open", file_path)
-  },
-  xmp: (file_path) => {
-    return ipcRenderer.invoke("xmp", file_path)
-  },
-  zoom: (ratio) => {
-    // ratio 50 - 200
-    if (ratio >= 50 && ratio <= 200) {
-      webFrame.setZoomFactor(ratio/100)
-    }
-  },
-  getzoom: () => {
-    // ratio 50 - 200
-    return webFrame.getZoomFactor()
-  },
-  docs: () => {
-    ipcRenderer.invoke("docs")
-  },
-  theme: (val) => {
-    ipcRenderer.invoke("theme", val)
-  },
-  debug: () => {
-    ipcRenderer.invoke("debug")
-  }
 })
